afc81020c7ff5fc2c4e636316a4ebff1ee4b59c3
yangzh <1214438717@qq.com>
2022-01-04 14:11:18 +0800

    feat: 增加axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,52 @@
-<script setup>
-
-</script>
-
 <template>
-   this is Home  // todo
+    <div class="about">
+        <!-- <v-header />
+        <v-sidebar /> -->
+        <div class="content-box" :class="{ 'content-collapse': collapse }">
+            <v-tags></v-tags>
+            <div class="content">
+                <router-view v-slot="{ Component }">
+                    <transition name="move" mode="out-in">
+                        <keep-alive :include="tagsList">
+                            <component :is="Component" />
+                        </keep-alive>
+                    </transition>
+                </router-view>
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+import { computed } from "vue";
+import  dataService  from "../utils/request"; // 直接引用axios 测试下
+// import vHeader from "../components/Header.vue";
+// import vSidebar from "../components/Sidebar.vue";
+// import vTags from "../components/Tags.vue";
+export default {
+    components: {
+      //   vHeader,
+      //   vSidebar,
+      //   vTags,
+    },
+    setup() {
+        const tagsList = computed(() =>{}
+            // store.state.tagsList.map((item) => item.name)
+        );
+        const collapse = computed(() => {});
+        dataService.get('/mytest').then(data=>
+        {
+               console.log(data);
+        }).catch(err=>{
+               console.error(err);
 
-<style>
+        });
 
-</style>
+
+
+        return {
+            tagsList,
+            collapse,
+        };
+    },
+};
+</script>
